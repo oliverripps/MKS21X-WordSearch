7da0dd7a0365bdbe17a6a1a2a21a765f39986417
Oliver Ripps <oripps@stuy.edu>
2018-11-09 19:01:00 -0500

    first constructor drafted

--- WordSearch.java
+++ WordSearch.java
@@ -2,23 +2,35 @@ import java.util.*; //random, scanner, arraylist
 import java.io.*; //file, filenotfoundexception
 public class WordSearch{
     private char[][]data;
+    //the random seed used to produce this WordSearch
+    private int seed;
+    //a random Object to unify your random calls
+    private Random randgen;
+    //all words from a text file get added to wordsToAdd, indicating that they have not yet been added
+    private ArrayList<String>wordsToAdd;
+    //all words that were successfully added get moved into wordsAdded.
+    private ArrayList<String>wordsAdded;
 
-        //the random seed used to produce this WordSearch
-        private int seed;
-
-        //a random Object to unify your random calls
-        private Random randgen;
-
-        //all words from a text file get added to wordsToAdd, indicating that they have not yet been added
-        private ArrayList<String>wordsToAdd;
-
-        //all words that were successfully added get moved into wordsAdded.
-        private ArrayList<String>wordsAdded;
-
-
-    public WordSearch( int rows, int cols, String fileName){
-      //choose a randSeed using the clock random
+    public WordSearch( int rows, int cols, String fileName) throws FileNotFoundException{
+      Random r = new Random();
+      int rn= r.nextInt() % 1000;
+      Random rng = new Random(rn);
+      seed= rng.nextInt();
+      char[][] n = new char[rows][cols];
+      data=n;
+      clear();
+      ArrayList<String> wordsToAdd = new ArrayList<String>();
+      try{
+      File f = new File(fileName);
+      Scanner p = new Scanner(f);
+      while( p.hasNext()){
+        String line = p.nextLine();
+        wordsToAdd.add(line);
+}
+    }catch(FileNotFoundException e){
+      System.out.println("File not found: " + fileName);
     }
+  }
     public WordSearch( int rows, int cols, String fileName, int randSeed){
       //  Both will read in the word text file, then run addAllWords(). Do not fill in random letters after.
 }
